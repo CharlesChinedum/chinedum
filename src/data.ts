69546122b2c61b895b4d388246559fa8c41d2a1d
CharlesChinedum <charleschinedum2@gmail.com>
2023-02-20 16:46:13 -0800

    Initial commit

---
+++ src/data.ts
@@ -0,0 +1,135 @@
+export const navLinks: string[] = ["Blog", "Projects", "About", "Contact"];
+
+export const navIcons: string[] = ["github", "twitter", "linkedin", "gmail"];
+
+export const tools = [
+  {
+    title: "React",
+    icon: "react",
+  },
+  {
+    title: "Next",
+    icon: "nextdotjs",
+  },
+  {
+    title: "Astro",
+    icon: "astro",
+  },
+  {
+    title: "Javascript",
+    icon: "javascript",
+  },
+];
+
+export const tools2 = [
+  {
+    title: "Typescript",
+    icon: "typescript",
+  },
+  {
+    title: "TailwindCss",
+    icon: "tailwindcss",
+  },
+  {
+    title: "Bootstrap",
+    icon: "bootstrap",
+  },
+  {
+    title: "Git",
+    icon: "git",
+  },
+];
+
+export const tools3 = [
+  {
+    title: "Github",
+    icon: "github",
+  },
+  {
+    title: "Linux",
+    icon: "linux",
+  },
+  {
+    title: "Sanity",
+    icon: "favicon.svg",
+  },
+];
+
+export const futureTools = [
+  {
+    title: "Rust",
+    icon: "rust",
+  },
+  {
+    title: "Solidity",
+    icon: "solidity",
+  },
+];
+
+export const projects = [
+  {
+    name: "Shoppy Dashboard",
+    img: "London2.jpg",
+    url: "",
+    github: "",
+    desc: "Reda allows you to import, organise, share and read your books, articles, and other digital documents anytime, anywhere.",
+  },
+  {
+    name: "Hoobank",
+    img: "London2.jpg",
+    url: "",
+    github: "",
+    desc: "Reda allows you to import, organise, share and read your books, articles, and other digital documents anytime, anywhere.",
+  },
+  {
+    name: "Fiber",
+    img: "London2.jpg",
+    url: "",
+    github: "",
+    desc: "Reda allows you to import, organise, share and read your books, articles, and other digital documents anytime, anywhere.",
+  },
+  {
+    name: "Jadoo",
+    img: "London2.jpg",
+    url: "",
+    github: "",
+    desc: "Reda allows you to import, organise, share and read your books, articles, and other digital documents anytime, anywhere.",
+  },
+  {
+    name: "Enaira",
+    img: "London2.jpg",
+    url: "",
+    github: "",
+    desc: "Reda allows you to import, organise, share and read your books, articles, and other digital documents anytime, anywhere.",
+  },
+  {
+    name: "Hosterr",
+    img: "London2.jpg",
+    url: "",
+    github: "",
+    desc: "Reda allows you to import, organise, share and read your books, articles, and other digital documents anytime, anywhere.",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Github",
+    icon: "github",
+    url: "https://github.com/CharlesChinedum",
+  },
+  {
+    title: "Twitter",
+    icon: "twitter",
+    url: "https://twitter.com/yvngKampa",
+  },
+  {
+    title: "LinkedIn",
+    icon: "linkedin",
+    url: "https://www.linkedin.com/in/charles-chinedum-40b1121a7",
+  },
+  {
+    title: "Email",
+    icon: "gmail",
+    url: "mailto:charleschinedum2@gmail.com",
+  },
+];
